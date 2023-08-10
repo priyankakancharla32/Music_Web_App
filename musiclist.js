4aec46386fc4f16fd87f029f6f3fca8fadbd6cd1
priyankakancharla32 <kancharlapriyanka32@gmail.com>
2023-08-10 07:34:30 +0530

    Developed the MusicApp

---
+++ musiclist.js
@@ -0,0 +1,34 @@
+
+let allMusic = [
+  {
+    name: "Fake A Smile",
+    artist: "Alan Walker, Salem Ilese",
+    img: "audio-1",
+    src: "music-1",
+  },
+  {
+    name: "Peace",
+    artist: "Tylor Swift",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "You - Benny",
+    artist: "Benny Blanco, Marshmello, Vance Joy",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Lifestyle feat. Adam Levine",
+    artist: " Jason Derulo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Cover Me In Sunshine",
+    artist: "P!nk, Willow Sage Hart",
+    img: "music-5",
+    src: "music-5",
+  },
+];
+
